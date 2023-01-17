92662393fab81175d2f1d50c46ca5ea88d456233
JamDev0 <jamdeveloper0@gmail.com>
2023-01-17 20:50:08 -0300

    Addition of new configurations to ESLint

--- packages/eslint-config/index.js
+++ packages/eslint-config/index.js
@@ -1,3 +1,9 @@
 module.exports = {
-  extends: ["@rocketseat/eslint-config/react"],
+  "extends": ["@rocketseat/eslint-config/react"],
+  'prettier/prettier': [
+    'error',
+    {
+      'endOfLine': 'auto',
+    }
+  ]
 }
