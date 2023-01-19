ce3f97a4f19da74c91660f01e1f49382baf939c7
JAMDev <61752887+JamDev0@users.noreply.github.com>
2023-01-18 23:32:33 -0300

    Final changes to deploy

--- packages/documentation/.storybook/main.js
+++ packages/documentation/.storybook/main.js
@@ -14,5 +14,12 @@ module.exports = {
   },
   "features": {
     "storyStoreV7": true
+  },
+  viteFinal: (config, { configType }) => {
+    if (configType === 'PRODUCTION') {
+      config.base = '/medical-registration-ddesign-system/'
+    }
+
+    return config
   }
 }
