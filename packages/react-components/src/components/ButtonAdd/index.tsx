8fa32f3d56dd13d236a2b5b76777191dfa8943f7
JamDev0 <jamdeveloper0@gmail.com>
2023-01-23 20:26:59 -0300

    removal of unused classNames in the ButtonAdd component

--- packages/react-components/src/components/ButtonAdd/index.tsx
+++ packages/react-components/src/components/ButtonAdd/index.tsx
@@ -7,7 +7,6 @@ interface ButtonProps extends ButtonHTMLAttributes<HTMLButtonElement> {}
 export function ButtonAdd({ ...props }: ButtonProps) {
   return (
     <ButtonAddContainer
-      className="px-4 text-brand font-semibold bg-transparent hover:bg-brand-50 rounded-full focus:outline-none focus:ring focus:ring-brand-50 flex justify-center items-center gap-2 p-1 border border-brand"
       {...props}
     >
       <Plus weight="fill" />
