1396f7cb9b4fcebd634a96746b3518e997fbc3cd
JamDev0 <jamdeveloper0@gmail.com>
2023-01-23 20:39:07 -0300

    Develop of the InputSearch component story

---
+++ packages/documentation/src/stories/Form/InputSearch.stories.tsx
@@ -0,0 +1,10 @@
+import { InputSearch, InputSearchProps } from "@medical-registration/react-components"
+import { Meta, StoryObj } from "@storybook/react"
+
+export default {
+  title: "Form/InputSearch",
+  component: InputSearch,
+  
+} as Meta<InputSearchProps>
+
+export const Primary: StoryObj<InputSearchProps> = {}
